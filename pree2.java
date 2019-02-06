36e036135352ffaf14b40822c5a95146c114cce8
Preethiselvam <47376744+Preethiselvam@users.noreply.github.com>
2019-02-06 00:50:24 -0800

    Create pree2.java

---
+++ pree2.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int x;
+	Scanner in=new Scanner(System.in);
+	x=in.nextInt();
+    if(x/2==0)
+{ 
+	System.out.println("Even");
+}
+else 
+{
+	System.out.println("Odd");
+}
+}
+}
