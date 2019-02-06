f7ed44ae129ad1d1b5dd5c272f23e22038282631
Preethiselvam <47376744+Preethiselvam@users.noreply.github.com>
2019-02-06 02:29:57 -0800

    Create pree3.java

---
+++ pree3.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+	char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            System.out.println( "vowel");
+        else
+            System.out.println( "consonant");
+	}
+}
